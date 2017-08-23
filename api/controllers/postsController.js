a891294c956f7c9c35097f9bb3bc593edfe8a5ce
Sean Chen <skypemaster007@gmail.com>
2017-08-23 16:11:16 -0700

    Add comments in controllers/postsController

--- api/controllers/postsController.js
+++ api/controllers/postsController.js
@@ -22,10 +22,16 @@ const addComment = (req, res) => {
 
 };
 
+// In this function, we need to delete the comment document
+// We also need to delete the comment's parent post's reference 
+// to the comment we just deleted
 const deleteComment = (req, res) => {
 
 };
 
+// Similarly, in this function we need to delete the post document,
+// along with any comments that are the children of this post
+// We don't want any orphaned children in our database
 const deletePost = (req, res) => {
 
 }; 
