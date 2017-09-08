7086f0e516c40669806a3ba10db03d741481a347
Ryan Hamblin <ryanhca@users.noreply.github.com>
2017-09-07 20:26:18 -0600

    updated for linter

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./api/routes/routes');
 
 const Post = require('./api/models/post');
 const Comment = require('./api/models/comment');
 
-const server  = express();
+const server = express();
 server.use(bodyParser.json());
 
 mongoose.Promise = global.Promise;
 const connect = mongoose.connect(
-    'mongodb://localhost/post-db', 
+    'mongodb://localhost/post-db',
     { useMongoClient: true }
 );
 
 connect.then(() => {
-    const port = 3000;
-    const routes = require('./api/routes/routes');
-    routes(server);
-    server.listen(3000);
-    console.log(`Server listening on port ${port}`);
+  const port = 3000;
+  routes(server);
+  server.listen(3000);
+  console.log(`Server listening on port ${port}`);
 }, (err) => {
-    console.log('\n**********************');
-    console.log("ERROR: Failed to connect to MongoDB.");
-    console.log('\n**********************');
+  console.log('\n**********************');
+  console.log('ERROR: Failed to connect to MongoDB.');
+  console.log('\n**********************');
 });
 
